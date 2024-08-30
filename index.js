b0689069c205f10b838f19d50153d2684b8cace6
Jose Torres <vidojam@gmail.com>
2024-08-30 12:14:06 -0400

    squares coding

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
 
-
+    for (let i = 0; i < squares.length; i++) {
+        squares(i).onclick = () => {
+            alert("You have clicked square" + i)
+        }
+    }
 })
